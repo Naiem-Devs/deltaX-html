8a32c9f497267bc813cd222ec3bfe9ab6f373533
Naiem-Devs <dnsnaiem@gmail.com>
2025-02-17 15:51:37 +0600

    all-done

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,29 +60,5 @@
 
 
 
-  // owlCarousel
-  $(".brand-active").owlCarousel({
-    loop: true,
-    margin: 30,
-    items: 6,
-    navText: [
-      '<i class="fa fa-angle-left"></i>',
-      '<i class="fa fa-angle-right"></i>'
-    ],
-    nav: false,
-    dots: false,
-    responsive: {
-      0: {
-        items: 2
-      },
-      767: {
-        items: 3
-      },
-      992: {
-        items: 6
-      }
-    }
-  });
-
  
 })(jQuery);
