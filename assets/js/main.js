69f3e75f6d7e9c570064ad68487076b2e871fb49
Naiem-Devs <dnsnaiem@gmail.com>
2025-02-17 14:56:33 +0600

    page-done

--- assets/js/main.js
+++ assets/js/main.js
@@ -84,20 +84,5 @@
     }
   });
 
-
-
-
-
-  // page Animation
-  // AOS.init({
-  //   mirror: true,
-  //   duration: 1500,
-  //   initClassName: 'aos-init',
-  //   once: true,
-  // });
-
-  // data-aos="fade-up" 
-  // data-aos-delay="300" 
-
  
 })(jQuery);
